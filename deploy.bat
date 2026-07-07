2703e77ba91446d6fb9ff57531b2d888bd9e690d
xsandy0506-art <xsandy0506@example.com>
2026-07-07 15:03:47 +0800

    Create deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d C:\Users\30128\Documents\hakka-lantern-h5\react-app
+"C:\Program Files\Git\bin\git.exe" add -A
+"C:\Program Files\Git\bin\git.exe" commit -m "Clean up conflicting configs, optimize build"
+"C:\Program Files\Git\bin\git.exe" push origin master
+pause
