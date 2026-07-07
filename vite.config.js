47f0d0bfaaa385e4507dae9bfc729880c15505ab
xsandy0506-art <xsandy0506@example.com>
2026-07-07 15:04:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  publicDir: 'public',
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'esbuild',
+  }
 })
